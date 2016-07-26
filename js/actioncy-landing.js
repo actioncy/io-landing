a0cd4033e7d035608a2b191ddc6cbab5189cd925
cpheimbach <christian-peter@heimba.ch>
2016-07-27 00:44:59 +0100

    end of day 1, structure complete

--- js/actioncy-landing.js
+++ js/actioncy-landing.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
 
+  $('nav').visibility({
+    onTopPassed: function() {
+      var navigationHTML = "<div class=\"ui borderless main menu\"><div class=\"ui text container\"><img class=\"logo\" src=\"img/actioncy-io-logo.png\"><button class=\"ui primary basic button right floated item\">Sign Up</button></div></div>";
+      $('nav').append(navigationHTML);
+    }
+  });
+
   $('.main.menu').visibility({
-    type: 'fixed'
+    type: 'fixed',
   });
 
   $('.overlay').visibility({
