94d593cceb6645fd74561b08065c1a9e13e2f159
cpheimbach <christian-peter@heimba.ch>
2016-07-28 16:00:34 +0100

    added mailchimp popup

--- js/actioncy-landing.js
+++ js/actioncy-landing.js
@@ -1,27 +1,28 @@
 $(document).ready(function() {
 
-  $('#mastheadCTA').visibility({
-    once: false,
-    onOnScreen: function() {
-      $('nav').hide();
-    },
-    onOffScreen: function() {
-      $('nav').show();
-    },
-  });
+    $('#mastheadCTA').visibility({
+        once: false,
+        onOnScreen: function() {
+            $('nav').hide();
+        },
+        onOffScreen: function() {
+            $('nav').show();
+        },
+    });
 
 
-  $('a[href^="#"]').on('click',function (e) {
-	    e.preventDefault();
+    $('a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
 
-	    var target = this.hash;
-	    var $target = $(target);
+        var target = this.hash;
+        var $target = $(target);
 
-	    $('html, body').stop().animate({
-	        'scrollTop': $target.offset().top
-	    }, 400, 'swing', function () {
-	        window.location.hash = target;
-	    });
-	});
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 400, 'swing', function() {
+            window.location.hash = target;
+        });
+    });
+
 
 });
